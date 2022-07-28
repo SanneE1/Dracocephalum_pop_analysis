3e8b5631ab8bba578a3cd8b80a33934a11f27dd3
Sanne <42802773+SanneE1@users.noreply.github.com>
2022-07-28 16:46:00 +0200

    model state independent vital rates

--- analysis/state_independent_variables.R
+++ analysis/state_independent_variables.R
@@ -1,8 +1,9 @@
 ## Script to estimate germination rate
-estimate_non_state_dependent_variables <- function(data_for_modeling) {
 
-data <- read.csv(data_for_modeling)
-  
+data <- read.csv('data/Dracocephalum_with_vital_rates.csv') %>%
+  rowwise() %>%
+  mutate(tot_shading_t0 = herb_shading_t0 + shrub_shading_t0)
+
 n_sdl_t3 <- data %>% filter(stage_t0 == "sdl") %>% 
   group_by(population, year_t0) %>% summarise(n_sdl_t3 = n()) %>%
   mutate(year_t0 = year_t0 - 3)
@@ -20,8 +21,8 @@ df <- plyr::join_all(list(n_seeds_t0, n_seeds_t1, n_seeds_t2, n_sdl_t3)) %>% ung
          ln_seeds_t0 = log(n_seeds_t0),
          ln_seeds_t1 = log(n_seeds_t1),
          ln_seeds_t2 = log(n_seeds_t2)
-         ) 
-  
+  ) 
+
 
 ### sdl_t2 normal distribution - tried a poisson distribution, but this had a higher n_eff
 germ_model <- rethinking::ulam(
@@ -37,20 +38,69 @@ germ_model <- rethinking::ulam(
   log_lik = T)
 
 
-sdl_data <- data %>% filter(stage_t0 == "sdl") %>%
-  mutate(n_stems_t1 = n_fl_stems_t1 + n_veg_stems_t1)
+sdl_data <- data %>% filter(stage_t0 == "sdl")
 
-## herb & shrub shading not significant for seedling survival, but herb_shading's CI does not span 0, so could be included
-sdl_surv <- glmer(survival_t1 ~ population + herb_shading_t0 + (1|year_t0), family = "binomial", data = sdl_data)
+## herb & shrub shading not significant for seedling survival and CI of estimates includes 0
+# sdl_surv <- glmer(survival_t1 ~ population + herb_shading_t0 + shrub_shading_t0 + (1|year_t0), family = "binomial", data = sdl_data)
+# confint.merMod(sdl_surv)
+sdl_surv <- glmer(survival_t1 ~ population + soil_depth + rock + slope + 
+                    tot_shading_t0 + (1|year_t0), 
+                  family = "binomial", 
+                  data = na.omit(sdl_data[c('survival_t1', 'population', 'soil_depth',
+                                          'rock', 'slope', 'tot_shading_t0', 'year_t0')])
+)
 
-## herb shading's CI does not spann 0, shrub shading's CI does include 0
-sdl_size_d <- lmer(n_stems_t1 ~ population + herb_shading_t0 + (1|year_t0), data = sdl_data %>% filter(survival_t1 == 1))
+surv_drop <- drop1(sdl_surv, test = "Chisq") %>% 
+  rownames_to_column("dropped_covariate") %>%
+  rename(p_value = `Pr(Chi)`) %>%
+  mutate(
+    p_adj_BH = p.adjust(p_value,method="BH",n=96),
+    p_adj_holm = p.adjust(p_value,method="holm",n=96),
+    include = ifelse(p_value < 0.05 & p_adj_BH < 0.05 & p_adj_holm < 0.5, "yes", "no")
+  ) %>% as_tibble()
 
 
+base_surv <- surv_drop %>% filter(include == "yes" | dropped_covariate == "ln_stems_t0") %>%
+  pull(dropped_covariate) %>% paste(., collapse = "+") %>%
+  paste0("survival_t1 ~", ., '(1|year_t0)')
 
-return(list(est_germination_rate = rethinking::extract.samples(germ_model, n = 1e5),
-            sdl_surv = sdl_surv,
-            sdl_size_d = sdl_size_d))
+sdl_surv <- glmer(formula(base_surv), 
+                  family = "binomial", data = sdl_data)
 
-}
 
+## herb & shrub shading not significant for seedling size distribution and CI of estimates includes 0
+# sdl_size_d <- lmer(ln_stems_t1 ~ population + herb_shading_t0 + shrub_shading_t0 + (1|year_t0), data = sdl_data %>% filter(survival_t1 == 1))
+# confint.merMod(sdl_size_d)
+sdl_size_d <- lmer(ln_stems_t1 ~ population + soil_depth + rock + slope + 
+                     tot_shading_t0 + (1|year_t0), 
+                   data = na.omit(sdl_data[c('survival_t1', 'ln_stems_t1', 'population', 
+                                             'soil_depth', 'rock', 'slope', 
+                                             'tot_shading_t0', 'year_t0')]) %>% 
+                                    filter(survival_t1 == 1)
+                   )
+
+sdld_drop <- drop1(sdl_size_d, test = "Chisq") %>% 
+  rownames_to_column("dropped_covariate") %>%
+  rename(p_value = `Pr(Chi)`) %>%
+  mutate(
+    p_adj_BH = p.adjust(p_value,method="BH",n=96),
+    p_adj_holm = p.adjust(p_value,method="holm",n=96),
+    include = ifelse(p_value < 0.05 & p_adj_BH < 0.05 & p_adj_holm < 0.5, "yes", "no")
+  ) %>% as_tibble()
+
+
+base_sdld <- sdld_drop %>% filter(include == "yes" | dropped_covariate == "ln_stems_t0") %>%
+  pull(dropped_covariate) %>% paste(., collapse = "+") %>%
+  paste0("ln_stems_t1 ~", ., '(1|year_t0)')
+
+sdl_size_d <- lmer(formula(base_sdld), 
+                   data = sdl_data %>% filter(survival_t1 == 1))
+
+
+SI_mods <- list(est_germination_rate = rethinking::extract.samples(germ_model, n = 1e5),
+                sdl_surv = sdl_surv,
+                sdl_size_d = sdl_size_d)
+
+saveRDS(SI_mods, file = "results/state_independent_VR.rds")
+
+rm(list = ls())
